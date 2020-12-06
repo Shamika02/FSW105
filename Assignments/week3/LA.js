453b6fe368402cf969fbc806ae6ffd8c4775fc8a
Shamika02 <shamika.sims@bryanuniversity.edu>
2020-12-06 10:24:30 -0500

    ...

--- Assignments/week3/LA.js
+++ Assignments/week3/LA.js
@@ -1,10 +1,17 @@
-var oldEnough = 18
-if (age < 18) {
-    message = "Your are old enough";
-  } else if (age > 18) {
-    message = "You are not old enough";
-  } else if (age < 100) {
-    message = 'Greetings!';
-  } else {
-    message = 'What an unusual age!';
-  }
+
+var age_ex= 18; 
+
+var age = [12, 80, 22, 30, 4];
+var person_Na = (" Mike,12,male" ,"Madeline,80,female" ,"Cheryl,22,female", "Sam,30,male" ,"Suzy,4,female");
+var gender_o = ("male", "female");
+
+if(age >= age_ex){
+        console.log (person_Na); console.log(" is old enough") ;
+          
+}
+    else{
+           console.log (person_Na); console.log("is  not old enough");
+        }
+         
+       
+
