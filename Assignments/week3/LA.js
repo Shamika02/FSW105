81a73ad46234c585871f0b917945321b54292b97
Shamika02 <shamika.sims@bryanuniversity.edu>
2020-12-06 05:45:35 -0500

    ...

--- Assignments/week3/LA.js
+++ Assignments/week3/LA.js
@@ -1,27 +1,10 @@
-var age = prompt("Please enter your age");
-		if(age>=18)
-		document.write("You are old enough <br />");
-		if(age<18)
-		document.write("You are not old enough <br />");
-
-    {
-        name: "Mike",
-        age: 12,
-        gender: "male"
-    },{
-        name: "Madeline",
-        age: 80,
-        gender: "female"
-    },{
-        name: "Cheryl",
-        age: 22,
-        gender: "female"
-    },{
-        name: "Sam",
-        age: 30,
-        gender: "male"
-    },{
-        name: "Suzy",
-        age: 4,
-        gender: "female"
-    }
+var oldEnough = 18
+if (age < 18) {
+    message = "Your are old enough";
+  } else if (age > 18) {
+    message = "You are not old enough";
+  } else if (age < 100) {
+    message = 'Greetings!';
+  } else {
+    message = 'What an unusual age!';
+  }
