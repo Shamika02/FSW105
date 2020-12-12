1956233d22378dea52c9e2dccacb62697a3edba0
Shamika02 <shamika.sims@bryanuniversity.edu>
2020-12-12 10:31:09 -0500

    ...

--- Assignments/week2/FS.js
+++ Assignments/week2/FS.js
@@ -1,4 +1,19 @@
-var num= 5
-    if (5 < 3) {
-      console.log("is greater than");
-    }
+//Write an if statement that prints "is greater than" if 5 is greater than 3
+var number = 5;
+  if (number > 3){
+console.log("is greater than")
+  }
+
+//Write an if statement that prints "is the length" if the length of "cat" is 3
+var length = 3;
+if (cat = 3){
+  console.log("is the length")
+}
+
+//Write an if/else statement that checks if "cat" is equal to "dog" and prints, "not the same" when they are not equal.
+var name = true;
+if (name = cat){
+  console.log("is equal to dog")
+} else{
+  console.log("is not the same")
+}
