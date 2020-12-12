fb28ad7e80b891c2e7822ce2b7d867286fe331cd
Shamika02 <shamika.sims@bryanuniversity.edu>
2020-12-12 14:48:40 -0500

    ...

---
+++ Assignments/week4/SM.js
@@ -0,0 +1,33 @@
+// Make a function that will return any given string into all caps followed by the same string all lowercase.
+var str = "Hello World";
+var res = str.toUpperCase();
+var resv= str.toLowerCase();
+console.log(res + resv)
+
+// Make a function that returns a number half the length and rounded down. 
+var str1 = "Hello"
+var str2 = "Hello World"
+console.log(Math.floor(5/2)); //5
+console.log(Math.floor(10/2)); //10
+
+
+
+// Make a function that uses slice() (Links to an external site.) and the other functions you've written to return the first half of the string.
+
+var str = "Happy Life";
+var res = str.slice(0, 5);
+console.log(res)
+
+
+//Make a function that takes a string and returns that string where the first half is capitalized, and the second half is lower cased. (If the string length is odd, capitalize the shorter of the first half.)
+var str = "Peace";
+var res = str.toUpperCase();
+var resv= str.toLowerCase();
+var whitespace =" ";
+console.log(res + whitespace + resv)
+
+
+
+
+
+
