d138d0c48b373474a2eff07db26d3428731676f4
Shamika02 <71770711+Shamika02@users.noreply.github.com>
2020-12-17 20:09:59 -0500

    ...

--- Assignments/week5/AEE.js
+++ Assignments/week5/AEE.js
@@ -1,19 +1,22 @@
 //Section A
 //1) Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
 var num1 = [2, 13, 6, 22,17,3,4];
-
 function checkSS(num1) {
   return num1 >= 5;
 }
-
 function myFunction() {
   document.getElementById("sham").innerHTML = num1.filter(checkSS);
 }
-
 //2) Given an array of numbers, return a new array that only includes the even numbers.
-
+let getEvenNumber =() =>{
+  var arr =[2, 13, 6, 22,17,3,4];
+  let the_evens = arr.filter(number => number %2 == 0);
+  document.write('Even Number:' + the_evens)
+}
+getEvenNumber();
 //Section B
 //1) Make an array of numbers that are doubles of the first array.
+
 
 //2) Take an array of numbers and make them strings.
 
